a4a212d9b41ab1673d19062037090090aa08eca4
Sang Vivianna Lee <vivianna.a.lee@gmail.com>
2019-11-08 11:28:07 -0500

    Update DB_tables_constraints.sql
    Reading up on some posts:
    https://stackoverflow.com/questions/1481476/when-to-use-on-update-cascade
    https://stackoverflow.com/questions/13444859/sql-on-delete-cascade-which-way-does-the-deletion-occur
    https://stackoverflow.com/questions/6720050/foreign-key-constraints-when-to-use-on-update-and-on-delete
    We had it kind of backwards, so I went in and fixed it. 
    
    
    Users: It might be best if we just do not allow users to be deleted from the system. 
    Employees: Taking out delete option. It should be default. Set it to inactive if they are no longer employed. 
    Order: Again, instead of delete cascade, in the status we can notify that it is a cancelled order.

--- DB_tables_constraints.sql
+++ DB_tables_constraints.sql
@@ -12,10 +12,10 @@ CREATE TABLE User (
   				password varchar(25)Not NULL,
   				phone varchar(10)Not NULL,
   				pid int(10)Not NULL,
+				admin varchar(1) Not NULL,
 				PRIMARY KEY (uid),
-				FOREIGN KEY(pid) REFERENCES Payments(pid),
-                		ON UPDATE CASCADE,
-				ON DELETE SET DEFAULT 'no user'); -- Maybe needs to be -1 or an int
+				FOREIGN KEY(pid) REFERENCES Payments(pid)
+                		ON UPDATE CASCADE);
 
  
 
@@ -27,19 +27,20 @@ CREATE TABLE Employees(
 			   lname VARCHAR(45)Not NULL,
 			   salary INT(11)Not NULL,   
 			   job_title VARCHAR(40)Not NULL,  
+			   active VARCHAR(1) not null,
  			   PRIMARY KEY(eid),
 			   FOREIGN KEY(sid) REFERENCES Stores(sid), 
 			   FOREIGN KEY(uid) REFERENCES Users(uid),
-		CHECK (salary > 0),
-               ON DELETE SET DEFAULT (-1); -- need to make sure we put in a reference to the default in the table
+			   CONSTRAINT validSalary
+			   CHECK (salary > 0)); 
 
 CREATE TABLE Regions(
     			 rid INT(11)Not NULL,
 			 rname VARCHAR(45)Not NULL,
 			 manager INT(11)Not NULL, 
 			 PRIMARY KEY(rid),
-			 FOREIGN KEY(manager) REFERENCES Employees(eid))
-             ON UPDATE CASCADE; 
+			 FOREIGN KEY(manager) REFERENCES Employees(eid)
+             		 ON UPDATE CASCADE); 
 
 
 
@@ -48,26 +49,24 @@ CREATE TABLE Stores(
 			 rid INT(11)Not NULL,
 			 sname VARCHAR(45)Not NULL,
 			 manager INT(11)Not NULL,  
+			 active VARCHAR(1) Not NULL,
 			 PRIMARY KEY(sid),
 			 FOREIGN KEY(rid) REFERENCES Regions(rid),
-			 FOREIGN KEY(manager) REFERENCES Employees(eid),
-		ON DELETE SET DEFAULT (-1), -- may need to pick a different default??? Like the online store?				   
-             ON UPDATE CASCADE); 
+			 FOREIGN KEY(manager) REFERENCES Employees(eid)		   
+             		 ON UPDATE CASCADE); 
 
 CREATE TABLE Authors (
   			 	auid INT(11)Not NULL,
   			 	fname varchar(45)Not NULL,
   			 	lname varchar(45)Not NULL,
-  			 	PRIMARY KEY (auid),
-	ON UPDATE CASCADE);
+  			 	PRIMARY KEY (auid));
 
 
 
 CREATE TABLE Genres ( 
   			 	gid INT(11)Not NULL,
   			 	gname varchar(45)Not NULL,
-  			 	PRIMARY KEY (gid),
-	ON UPDATE CASCADE);
+  			 	PRIMARY KEY (gid));
 
 
 
@@ -76,8 +75,8 @@ CREATE TABLE Publishers (
   				pname varchar(45)Not NULL,
 				cid INT(11)Not NULL,
 				PRIMARY KEY (pubid),
-				FOREIGN KEY(cid) REFERENCES Cities(cid),
-                ON UPDATE CASCADE);
+				FOREIGN KEY(cid) REFERENCES Cities(cid)
+               		 	ON UPDATE CASCADE);
 
 CREATE TABLE Books_genres (
   				isbn INT(13)Not NULL,
@@ -86,7 +85,7 @@ CREATE TABLE Books_genres (
   				PRIMARY KEY (gid,isbn));
 
 CREATE TABLE Orders(
-            oid INT(11)Not NULL,
+           		 oid INT(11)Not NULL,
 			 aid INT(11)Not NULL,   
 			 eid INT(11)Not NULL,   
 			 order_date DATE,
@@ -94,10 +93,10 @@ CREATE TABLE Orders(
 			 sid INT(11)Not NULL,
 			 PRIMARY KEY(oid),
 			 FOREIGN KEY(aid) REFERENCES Addresses(aid),
-			 FOREIGN KEY(eid) REFERENCES Employees(eid),
-		CHECK(shipped_date > order_date),
-		ON DELETE SET DEFAULT (-1), -- As usual, may need to change the default
-             ON UPDATE CASCADE);
+			 FOREIGN KEY(eid) REFERENCES Employees(eid) ON UPDATE CASCADE,
+			 CONSTRAINT correctDate
+			 CHECK (shipped_date > order_date);
+
 
 
 CREATE TABLE Orders_books(
@@ -106,27 +105,27 @@ CREATE TABLE Orders_books(
 			      units INT(11)Not NULL,
 			      price_at_sale INT,
 			      PRIMARY KEY(oid, isbn),
-			      FOREIGN KEY(oid) REFERENCES Orders(oid),
-			      FOREIGN KEY(isbn) REFERENCES Books(isbn))
+			      FOREIGN KEY(oid) REFERENCES Orders(oid) ON UPDATE CASCADE,
+			      FOREIGN KEY(isbn) REFERENCES Books(isbn) ON UPDATE CASCADE);
  
 
 CREATE TABLE Users_orders(
 				uid VARCHAR(45)Not NULL,
 				oid INT(11)Not NULL,
 				PRIMARY KEY(uid, oid),
-				FOREIGN KEY (uid) REFERENCES Users(uid),
-				FOREIGN KEY (oid) REFERENCES Orders(oid))
+				FOREIGN KEY (uid) REFERENCES Users(uid) ON UPDATE CASCADE,
+				FOREIGN KEY (oid) REFERENCES Orders(oid) ON UPDATE CASCADE)
 
 
 
 
 
 CREATE TABLE In_cart(
-                uid VARCHAR(45)Not NULL,
+                		uid VARCHAR(45)Not NULL,
 			 	isbn INT(13)Not NULL,
 			 	PRIMARY KEY(uid, isbn),
-			 	FOREIGN KEY(uid) REFERENCES Users(uid),
-			 	FOREIGN KEY(isbn) REFERENCES Books(isbn));
+			 	FOREIGN KEY(uid) REFERENCES Users(uid) ON UPDATE CASCADE,
+			 	FOREIGN KEY(isbn) REFERENCES Books(isbn) ON UPDATE CASCADE);
               
 
 
@@ -140,9 +139,8 @@ CREATE TABLE Books (
   			 	price double(10,4)Not NULL,
   			 	quantity_avail INT Not NULL,
   			 	PRIMARY KEY (isbn),
-  			 	FOREIGN KEY(pubid) REFERENCES Publishers(pubid),
-  			 	FOREIGN KEY(auid) REFERENCES Authors (auid),
-                ON UPDATE CASCADE);
+  			 	FOREIGN KEY(pubid) REFERENCES Publishers(pubid) ON UPDATE CASCADE,
+  			 	FOREIGN KEY(auid) REFERENCES Authors (auid) ON UPDATE CASCADE);
 
 
 
@@ -152,14 +150,14 @@ CREATE TABLE Favorites (
   				isbn INT(13)Not NULL,
   				PRIMARY KEY (uid,isbn),
 				FOREIGN KEY(uid) REFERENCES Users(uid),
-				FOREIGN KEY(isbn) REFERENCES Books(isbn));
+				FOREIGN KEY(isbn) REFERENCES Books(isbn) ON UPDATE CASCADE);
 
 CREATE TABLE Wishlists (
   				uid varchar(45)Not NULL,
   				isbn INT(13)Not NULL,
   				PRIMARY KEY (uid,isbn),
 				FOREIGN KEY(uid) REFERENCES Users(uid),
-				FOREIGN KEY(isbn) REFERENCES Books(isbn));
+				FOREIGN KEY(isbn) REFERENCES Books(isbn) ON UPDATE CASCADE);
 
 CREATE TABLE Ratings (
   				uid varchar(45)Not NULL,
@@ -168,15 +166,14 @@ CREATE TABLE Ratings (
 				review text,
   				PRIMARY KEY (uid,isbn),
 				FOREIGN KEY(uid) REFERENCES Users(uid),
-				FOREIGN KEY(isbn) REFERENCES Books(isbn));
+				FOREIGN KEY(isbn) REFERENCES Books(isbn) ON UPDATE CASCADE);
 
 
 CREATE TABLE Cities (
   				cid INT(11)Not NULL,
   				cname varchar(50)Not NULL,
   				state varchar(25)Not NULL,
-  				PRIMARY KEY(cid)
-	ON UPDATE CASCADE);
+  				PRIMARY KEY(cid));
 
 
 CREATE TABLE Addresses (
@@ -185,15 +182,14 @@ CREATE TABLE Addresses (
  				cid INT(11)Not NULL,
   				zip varchar(15)Not NULL,
   				PRIMARY KEY (aid),
-				FOREIGN KEY(cid) REFERENCES Cities(cid),
-	ON UPDATE CASCADE);
+				FOREIGN KEY(cid) REFERENCES Cities(cid) ON UPDATE CASCADE);
 
 CREATE TABLE User_addresses (
   				uid varchar(45)Not NULL,
   				aid INT(11)Not NULL, 
  				PRIMARY KEY (uid,aid),
-  				FOREIGN KEY(uid) REFERENCES Users(uid), 
-  				FOREIGN KEY(aid) REFERENCES Addresses(aid));
+  				FOREIGN KEY(uid) REFERENCES Users(uid) ON UPDATE CASCADE, 
+  				FOREIGN KEY(aid) REFERENCES Addresses(aid) ON UPDATE CASCADE);
 
 
  
